10ee036f55922fb2867e6b9db3d6ab163a9ccb04
Estudiantes <avila.avarez@gmail.com>
2024-02-19 07:35:33 -0500

    Primera actualizacion

--- cartas.py
+++ cartas.py
@@ -3,12 +3,15 @@ class Carta:
         self.valor = valor
         self.pinta = pinta
 
-    def def_valor(self):
+    def dar_valor(self):
         if self.valor in ['J', 'Q', 'K']:
             return 10
         if self.valor == 'A':
             return 1
         return int(self.valor)
+    
+    def mostrar(self):
+        return self.valor + " " + self.pinta
     
 class Mazo:
     def __init__(self, jugador = False):
@@ -16,9 +19,28 @@ class Mazo:
             self.cartas = []
         else:
             self.cartas = [Carta(v,p)
-                           for v in ['A', 'J', 'Q', 'K']+[str(x) for x in range(1,11)]
+                           for v in ['A', 'J', 'Q', 'K']+[str(x) for x in range(2,11)]
                            for p in ['picas', 'treboles', 'corazones', 'diamantes']]
+    
+    def dar_valor(self):
+        valor = 0
+        for c in self.cartas:
+            valor += c.dar_valor()
+        return valor
+    
+    def dar_carta(self):
+        return self.cartas.pop()
+    
+    def agregar_carta(self, carta):
+        self.cartas.append(carta)
 
     
-#if __name__ == '__main__':
-#   c = Carta()
+if __name__ == '__main__':
+    m = Mazo()
+    j = Mazo(True)
+    j.agregar_carta(m.dar_carta())
+    j.agregar_carta(m.dar_carta())
+    for c in m.cartas:
+        print(c.mostrar())
+    print(m.dar_valor())
+    print(j.dar_valor())
