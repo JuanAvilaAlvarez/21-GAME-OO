de96e81d6165fb7bbfd3daf3a73575aee96e827e
Edu-20241 <demunoz05@gmail.com>
2024-02-21 06:36:31 -0500

    Creacion del archivo juego

--- cartas.py
+++ cartas.py
@@ -1,3 +1,5 @@
+import random
+
 class Carta:
     def __init__(self, valor, pinta):
         self.valor = valor
@@ -21,18 +23,35 @@ class Mazo:
             self.cartas = [Carta(v,p)
                            for v in ['A', 'J', 'Q', 'K']+[str(x) for x in range(2,11)]
                            for p in ['picas', 'treboles', 'corazones', 'diamantes']]
+            random.shuffle(self.cartas)
     
     def dar_valor(self):
         valor = 0
         for c in self.cartas:
             valor += c.dar_valor()
+        if self.tiene_as() and valor <= 11:
+            valor += 10
         return valor
+    
+    def tiene_as(self):
+        for c in self.cartas:
+            if c.valor == "A":
+                return True
+        return False
     
     def dar_carta(self):
         return self.cartas.pop()
     
     def agregar_carta(self, carta):
         self.cartas.append(carta)
+    
+    def mostrar_cartas(self, todas = False):
+        if todas:
+            print(self.cartas[0].mostrar())
+        else:
+            print(" de *")
+        for c in self.cartas[1:]:
+            print(c.mostrar())
 
     
 if __name__ == '__main__':
@@ -40,7 +59,6 @@ if __name__ == '__main__':
     j = Mazo(True)
     j.agregar_carta(m.dar_carta())
     j.agregar_carta(m.dar_carta())
-    for c in m.cartas:
-        print(c.mostrar())
-    print(m.dar_valor())
+    j.agregar_carta(Carta("A","picas"))
+    j.mostrar_cartas(True)
     print(j.dar_valor())
